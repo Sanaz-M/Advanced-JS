9db0fa25c27b77d2e96e9fa7c381077fc9c5f68c
Sanaz M <sanazmohammaddi@gmail.com>
2021-10-28 15:38:18 +0100

    shopping cart

--- M3-D4.js
+++ M3-D4.js
@@ -1,15 +1,37 @@
+// Book model:
+// asin: "1940026091"
+// category: "scifi"
+// img: "https://images-na.ssl-images-amazon.com/images/I/91xrEMcvmQL.jpg"
+// price: 7.81
+// title: "Pandemic (The Extinction Files, Book 1)"
+// Template literal example:
+// `<div>Title: ${books[i].title}</div>`
+
+// let node = document.createElement("div");
+            // node.classList.add("card col-2");
+            // let cardImag= document.createElement('img');
+            // cardImag.src = body[i].img
+            // node.appendChild('img'); 
+
 window.onload = () =>{
     fetch("https://striveschool-api.herokuapp.com/books")
 
     .then((response) =>{
-        console.log(response)
         return response.json()
     })
-    
-    .then((body) => {
-        console.log(body)
-        const booksInfo = document.querySelectorAll('.cards')
-        
+    .then((arrayOfBooks) => {
+        const booksInfo = document.getElementById('books-container');
+        for (let bookIndex=0; bookIndex<arrayOfBooks.length; bookIndex++){
+            booksInfo.innerHTML +=  `<div class="col-3"> <div class="card m-3">
+            <img src="${arrayOfBooks[bookIndex].img}"   class="card-img-top" alt="...">
+            <div class="card-body">
+             <h5 class="card-title">${arrayOfBooks[bookIndex].title}</h5>
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+           </div>
+           </div>
+           </div>`            
+        }
     })
 
 }
