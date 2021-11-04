bbe4bb2f4e6e818b09b036dd43ac6c3364ad7b16
Sanaz M <sanazmohammaddi@gmail.com>
2021-11-04 17:17:24 +0000

    updated

---
+++ D8-D9/M3-D9.js
@@ -0,0 +1,57 @@
+const url = 'https://striveschool-api.herokuapp.com/api/product';
+
+
+
+const fetchProducts = async () => {
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGJmM2FhY2FhMjAwMTU1MmExNmQiLCJpYXQiOjE2MzU5NDU0NTksImV4cCI6MTYzNzE1NTA1OX0.68CC8Jf4IHn7VZW39FPf-bHEv8MKux00DbaR2yT026Y"
+            },
+        });
+        if (response.ok) {
+            const res = await response.json()
+            return res
+        }
+        else{
+            console.log(error)
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+const displayProduct = (products) => {
+
+    console.log(products)
+    const cards = document.getElementById('productsCards')
+
+    products.forEach(product => {
+        cards.innerHTML += `<div class="card mx-2 mb-3" style="width: 150px; height: 200px;">
+                                <img src="${product.imageURL}" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                <h5 class="card-title">${product.name}</h5>
+                                <p class="card-text">{${product.description}}</p>
+                                <a href="#" id="more-details">More Details</a>
+                              </div>
+                            </div>`
+    });
+
+}
+
+
+window.onload = async () => {
+
+    try {
+
+        const products = await fetchProducts()
+        displayProduct(products)
+         
+    }
+    catch (error) {
+        alert(error)
+    }
+}
